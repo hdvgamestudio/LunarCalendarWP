4b2351d85bdadd0d77fa10c8a480f5947338a001
huypq <thevinh92@gmail.com>
2015-08-10 17:46:20 +0700

    Setup develop

--- LunarCalendar/LunarCalendar/App.xaml.cs
+++ LunarCalendar/LunarCalendar/App.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
+using CSMT.LunarCalendar.PresentationLayer.Pages;
+
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace LunarCalendar
@@ -90,7 +92,7 @@ namespace LunarCalendar
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(CalendarPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
